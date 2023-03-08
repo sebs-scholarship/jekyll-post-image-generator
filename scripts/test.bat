a7ed1c6b5e166ea9ccdc765873e781ab3be011b4
Levi Muniz <levi.muniz17@gmail.com>
2023-03-07 17:22:57 -0700

    chore: Add platform to test scripts

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,4 +1,4 @@
-docker run -v %cd%:/apps --rm alpine/bundle:2.7.0 ^
+docker run --platform linux/amd64 -v %cd%:/apps --rm alpine/bundle:2.7.0 ^
 /bin/bash -c "gem install bundler:2.4.2 ^
 && bundle config --local path 'vendor/bundle' ^
 && bundle install ^
