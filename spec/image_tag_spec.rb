4ecaf798de831f4b1cb45720187ff32822f7a4bb
Levi Muniz <levi.muniz17@gmail.com>
2024-01-18 01:22:09 -0700

    test: Add test to check that cover image is not returned on "empty" values

--- spec/image_tag_spec.rb
+++ spec/image_tag_spec.rb
@@ -33,6 +33,47 @@ describe(Jekyll::JekyllPostImageGenerator::ImageGenerator) do
         file.unlink
       end
     end
+
+    RSpec.shared_examples 'no cover image' do |value|
+      it 'should not return path to cover image' do
+        site = Site.new(source_dir)
+        output_dir = Jekyll::JekyllPostImageGenerator::DEFAULTS['output_directory']
+        expanded = File.expand_path(output_dir)
+        FileUtils.mkdir_p(expanded)
+        file = Tempfile.new(['', '.jpg'], expanded)
+
+        begin
+          file.close
+          path = file.path
+          raise('Temp file not created properly') if path.nil?
+
+          name = File.basename(path)
+          doc = Document.new(name[0...-4], "#{source_dir}/#{name}")
+          doc.data = doc.data.merge({ 'title' => 'test', 'cover_image' => value })
+          site.posts.docs << doc
+          parse_context = Liquid::ParseContext.new
+          parse_context.line_number = 1
+          image_tag = Jekyll::ImageTag.parse('', '', '', parse_context)
+          context = Context.new({ :site => site }, { 'page' => { 'path' => "#{source_dir}/#{name}" } }) # rubocop:disable Style/HashSyntax
+
+          expect(image_tag.render(context)).not_to eql(value)
+        ensure
+          file.unlink
+        end
+      end
+    end
+  end
+
+  context 'when empty cover image is set' do
+    include_examples('no cover image', '')
+  end
+
+  context 'when nil cover image is set' do
+    include_examples('no cover image', nil)
+  end
+
+  context 'when blank cover image is set' do
+    include_examples('no cover image', '  ')
   end
 
   context 'when post image is not rendered' do
